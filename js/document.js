05ca55da7524c7d6e4ec92a18b5de5544f6f2996
SKARABI <skarabifounder@gmail.com>
2018-02-03 20:43:00 +0000

    Some modifications few of them

--- js/document.js
+++ js/document.js
@@ -1,3 +1,4 @@
-$(document).ready(function(){
-    $('.materialboxed').materialbox();
-});
+// Initialize collapse button
+$(".button-collapse").sideNav();
+// Initialize collapsible (uncomment the line below if you use the dropdown variation)
+//$('.collapsible').collapsible();
