8364b99ccea31186c1fd8965ad2dd6e12ad9a3d9
SKARABI <skarabifounder@gmail.com>
2018-02-03 22:32:13 +0000

    Show messages while scrolling with Jquery

--- js/document.js
+++ js/document.js
@@ -5,4 +5,7 @@ $(document).ready(function(){
     /* SWITCH BETWEEN IMAGES IN THE CAROUSEL*/
     $('.carousel').carousel();
 
+    /* SHOW MESSAGES WHILE SCROLLING*/
+    var options = [{selector: '.card', offset: 500, callback: function(el) { Materialize.toast("This is our ScrollFire Card!", 1500 ); } }, {selector: '.card', offset: 5000, callback: function(el) { Materialize.toast("Please continue scrolling!", 1500 ); } }, {selector: '.card', offset: 400, callback: function(el) { Materialize.showStaggeredList($(el)); } }, {selector: '.card', offset: 5000, callback: function(el) { Materialize.fadeInImage($(el)); } } ]; Materialize.scrollFire(options);
+
 });
