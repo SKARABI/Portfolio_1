e062f3679d64d2ce17f7edda02e52b8bda0f03b4
SKARABI <skarabifounder@gmail.com>
2018-02-03 22:03:20 +0000

    I add a folder Carousel and I add some codes of Jquery

--- js/document.js
+++ js/document.js
@@ -1,4 +1,8 @@
-/*I ZOOM THE PICTURES IN THE CARDS*/
+    /*I ZOOM THE PICTURES IN THE CARDS*/
 $(document).ready(function(){
     $('.materialboxed').materialbox();
+
+    /* SWITCH BETWEEN IMAGES IN THE CAROUSEL*/
+    $('.carousel').carousel();
+
 });
